0749b892730dcd32efed3ffa52d9572b58d880b3
vigneshp <vigneshp@ELPBLR-049.evolvus.com>
2020-03-02 15:15:03 +0530

    yu

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 		
 		
-		System.out.println("Hello  world");
+		System.out.println("Hello   world");
 	}
 
 }
